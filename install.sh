b9e60a3f8bc06ab02ce81f02d453e90de9a0e94c
Brian Ballsun-Stanton <brian@fedarch.org>
2017-05-18 14:36:15 +1000

    16.04

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo apt-get install spatialite-bin libimage-exiftool-perl imagemagick scantailor libtiff-tools pdftk tesseract-ocr exactimage poppler-utils -y
+sudo apt-get install spatialite-bin libimage-exiftool-perl imagemagick scantailor libtiff-tools pdftk tesseract-ocr exactimage poppler-utils pandoc -y
