4d02e67c8bd702375249bade901aaf6906efddb8
Brian Ballsun-Stanton <brian@fedarch.org>
2017-05-19 11:44:14 +1000

    argh

--- stitchPDF.sh
+++ stitchPDF.sh
@@ -132,12 +132,16 @@ sed -i 's/\[letter\]/\[A4\]/g' "${3}cover.tex"
 
 context "${3}cover.tex" --purgeall --quiet --batchmode > /dev/null
 
-pdfunite "${3}cover.pdf" "${3}_preoriginal.pdf" "../${3}_original.pdf"
-pdfunite "${3}cover.pdf" "${3}_preOCR.pdf" "../${3}_OCR.pdf"
-cd ..
-cat stage2/$3.md "stage2/${3}_ENG.txt" > "${3}_ENG.txt"
+pdfunite "${3}cover.pdf" "${3}_preoriginal.pdf" "${3}_originalfull.pdf"
+pdfunite "${3}cover.pdf" _i"${3}_preOCR.pdf" "../${3}_OCR.pdf"
 
-# pdftk "${3}_originalfull.pdf" dump_data > "${3}prefull".info 2>/dev/null
+
+pdftk "${3}_originalfull.pdf" dump_data output "${3}prefull".info 2>/dev/null
+sed -i '/^Info/d' "${3}prefull.info"
+cat "$3.info" "${3}prefull.info" > "${3}full.info"
+pdftk "${3}_originalfull.pdf" update_info_utf8 "${3}full.info" output "../$3_original.pdf" 
+
+
 # pdftk "${3}_OCRfull.pdf" dump_data > "${3}preOCRfull".info 2>/dev/null
 # cat "${3}prefull.info"
 # sed -i '/^Info/d' "${3}prefull.info"
@@ -159,6 +163,8 @@ cat stage2/$3.md "stage2/${3}_ENG.txt" > "${3}_ENG.txt"
 # echo "ocr"
 # pdftk "stage2/${3}_OCRfull.pdf" update_info_utf8 "stage2/${3}OCRfull.info" output "$3_OCR.pdf" 
 # echo $?
+cd ..
+cat stage2/$3.md "stage2/${3}_ENG.txt" > "${3}_ENG.txt"
 
 rm -rf stage2
 exit 0
