a6480b1b5d66766564ae1518dd9c734b7d13c053
Brian Ballsun-Stanton <brian@fedarch.org>
2017-06-30 17:53:41 +1000

    comma

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
-sudo add-apt-repository -y ppa:reviczky/context-daily
-sudo apt-get update
-sudo apt-get install spatialite-bin libimage-exiftool-perl imagemagick scantailor libtiff-tools pdftk tesseract-ocr exactimage poppler-utils pandoc context parallel -y
+
+sudo apt-get install spatialite-bin libimage-exiftool-perl imagemagick scantailor libtiff-tools pdftk tesseract-ocr exactimage poppler-utils pandoc parallel -y
 sudo mkdir -p /home/ubuntu/.parallel
 sudo touch /home/ubuntu/.parallel/will-cite
 sudo mkdir -p /root/.parallel
 sudo touch /root/.parallel/will-cite
+
+sudo mkdir /usr/local/context
+sudo chown ubuntu /usr/local/context
+cd /usr/local/context
+wget http://minimals.contextgarden.net/setup/first-setup.sh
+sh first-setup.sh --modules=all
+
+
+cat <<-HereDoc > "/home/ubuntu/.bashrc"
+ export OSFONTDIR=~/.fonts:/usr/share/fonts                                                      
+ export TEXROOT=/usr/local/context/tex                                         
+ export PATH=/usr/local/context/tex/texmf-linux/bin:/usr/local/context/bin:$PATH
+HereDoc
